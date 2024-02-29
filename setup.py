3917bf1f44d3afecf3d26d155ba15a819f4f5080
braib <102415109+braib@users.noreply.github.com>
2024-02-29 15:34:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                'talker = py_pubsub.publisher_member_function:main',
-                'listener = py_pubsub.subscriber_member_function:main',
+                'talker = pubsub.publisher:main',
+                'listener = pubsub.subscriber:main',
         ],
     },
 )
